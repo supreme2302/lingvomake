5e25a9cfec2a54ae3318d27b4ec2fbb302245f45
supreme2302 <chuvashov.egor@mail.ru>
2019-05-01 20:24:34 +0300

    add todo

--- src/router/guards/coursesGuard.js
+++ src/router/guards/coursesGuard.js
@@ -1,7 +1,8 @@
-import store from '@/store/index.js'
+import store from '@/store/index.js';
 
 export default (to, from, next) => {
   console.log('call courses guard');
+  //todo проверять авторизацию
   store.dispatch('loadCourses')
       .then(() => next())
       .catch(() => next('/'));
