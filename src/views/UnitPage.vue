48b0b172528b874b2bdd9d68beae03b5d4d04b22
supreme2302 <chuvashov.egor@mail.ru>
2019-05-11 23:37:49 +0300

    - tags for testing

--- src/views/UnitPage.vue
+++ src/views/UnitPage.vue
@@ -132,82 +132,82 @@
                                       :rules="unitDescriptionRules"
                                       label="Unit Description"/>
                           </v-flex>
-                          <v-flex>
-                              <v-combobox
-                                      v-model="chipModel"
-                                      :rules="chipRules"
-                                      :filter="filter"
-                                      :hide-no-data="!chipSearch"
-                                      :items="chipItems"
-                                      :search-input.sync="chipSearch"
-                                      hide-selected
-                                      label="Unit tags"
-                                      multiple
-                                      small-chips
-                                      solo>
+                          <!--<v-flex>-->
+                              <!--<v-combobox-->
+                                      <!--v-model="chipModel"-->
+                                      <!--:rules="chipRules"-->
+                                      <!--:filter="filter"-->
+                                      <!--:hide-no-data="!chipSearch"-->
+                                      <!--:items="chipItems"-->
+                                      <!--:search-input.sync="chipSearch"-->
+                                      <!--hide-selected-->
+                                      <!--label="Unit tags"-->
+                                      <!--multiple-->
+                                      <!--small-chips-->
+                                      <!--solo>-->
 
-                                  <template v-slot:no-data>
-                                      <v-list-tile>
-                                          <span class="subheading">Create</span>
-                                          <v-chip
-                                                  :color="`${chipColors[chipNonce - 1]} lighten-3`"
-                                                  label
-                                                  small>
-                                              {{ chipSearch }}
-                                          </v-chip>
-                                      </v-list-tile>
-                                  </template>
+                                  <!--<template v-slot:no-data>-->
+                                      <!--<v-list-tile>-->
+                                          <!--<span class="subheading">Create</span>-->
+                                          <!--<v-chip-->
+                                                  <!--:color="`${chipColors[chipNonce - 1]} lighten-3`"-->
+                                                  <!--label-->
+                                                  <!--small>-->
+                                              <!--{{ chipSearch }}-->
+                                          <!--</v-chip>-->
+                                      <!--</v-list-tile>-->
+                                  <!--</template>-->
 
-                                  <template v-slot:selection="{ item, parent, selected }">
-                                      <v-chip
-                                              v-if="item === Object(item)"
-                                              :color="`${item.color} lighten-3`"
-                                              :selected="selected"
-                                              label
-                                              small>
-                              <span class="pr-2">
-                                {{ item.text }}
-                              </span>
-                                          <v-icon
-                                                  small
-                                                  @click="parent.selectItem(item)">close
-                                          </v-icon>
-                                      </v-chip>
-                                  </template>
+                                  <!--<template v-slot:selection="{ item, parent, selected }">-->
+                                      <!--<v-chip-->
+                                              <!--v-if="item === Object(item)"-->
+                                              <!--:color="`${item.color} lighten-3`"-->
+                                              <!--:selected="selected"-->
+                                              <!--label-->
+                                              <!--small>-->
+                              <!--<span class="pr-2">-->
+                                <!--{{ item.text }}-->
+                              <!--</span>-->
+                                          <!--<v-icon-->
+                                                  <!--small-->
+                                                  <!--@click="parent.selectItem(item)">close-->
+                                          <!--</v-icon>-->
+                                      <!--</v-chip>-->
+                                  <!--</template>-->
 
-                                  <template v-slot:item="{ index, item }">
-                                      <v-list-tile-content>
-                                          <v-text-field
-                                                  v-if="editing === item"
-                                                  v-model="editing.text"
-                                                  autofocus
-                                                  flat
-                                                  background-color="transparent"
-                                                  hide-details
-                                                  solo
-                                                  @keyup.enter="edit(index, item)">
-                                          </v-text-field>
-                                          <v-chip
-                                                  v-else
-                                                  :color="`${item.color} lighten-3`"
-                                                  dark
-                                                  label
-                                                  small>
-                                              {{ item.text }}
-                                          </v-chip>
-                                      </v-list-tile-content>
-                                      <v-spacer></v-spacer>
-                                      <v-list-tile-action @click.stop>
-                                          <v-btn
-                                                  icon
-                                                  @click.stop.prevent="edit(index, item)">
-                                              <v-icon>{{ editing !== item ? 'edit' : 'check' }}</v-icon>
-                                          </v-btn>
-                                      </v-list-tile-action>
-                                  </template>
+                                  <!--<template v-slot:item="{ index, item }">-->
+                                      <!--<v-list-tile-content>-->
+                                          <!--<v-text-field-->
+                                                  <!--v-if="editing === item"-->
+                                                  <!--v-model="editing.text"-->
+                                                  <!--autofocus-->
+                                                  <!--flat-->
+                                                  <!--background-color="transparent"-->
+                                                  <!--hide-details-->
+                                                  <!--solo-->
+                                                  <!--@keyup.enter="edit(index, item)">-->
+                                          <!--</v-text-field>-->
+                                          <!--<v-chip-->
+                                                  <!--v-else-->
+                                                  <!--:color="`${item.color} lighten-3`"-->
+                                                  <!--dark-->
+                                                  <!--label-->
+                                                  <!--small>-->
+                                              <!--{{ item.text }}-->
+                                          <!--</v-chip>-->
+                                      <!--</v-list-tile-content>-->
+                                      <!--<v-spacer></v-spacer>-->
+                                      <!--<v-list-tile-action @click.stop>-->
+                                          <!--<v-btn-->
+                                                  <!--icon-->
+                                                  <!--@click.stop.prevent="edit(index, item)">-->
+                                              <!--<v-icon>{{ editing !== item ? 'edit' : 'check' }}</v-icon>-->
+                                          <!--</v-btn>-->
+                                      <!--</v-list-tile-action>-->
+                                  <!--</template>-->
 
-                              </v-combobox>
-                          </v-flex>
+                              <!--</v-combobox>-->
+                          <!--</v-flex>-->
                           <v-flex xs12 text-xs-right>
                               <v-btn
                                       class="mx-0 font-weight-light"
