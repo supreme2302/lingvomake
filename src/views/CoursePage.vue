109ac7bd7f7e48aae15a6d983c1ec2207f524596
supreme2302 <chuvashov.egor@mail.ru>
2019-05-12 16:41:00 +0300

    remove tooltips

--- src/views/CoursePage.vue
+++ src/views/CoursePage.vue
@@ -53,12 +53,7 @@
                   icon style="position: absolute; top:2%; right:2%"
                   @click="openDeleteCourseDialog"
           >
-            <v-tooltip bottom>
-              <template v-slot:activator="{ on }">
-                <v-icon color="red" dark v-on="on">delete</v-icon>
-              </template>
-              <span>Delete</span>
-            </v-tooltip>
+            <v-icon color="red" dark>delete</v-icon>
           </v-btn>
         </material-card>
       </v-flex>
@@ -125,82 +120,30 @@
                         :rules="courseOrUnitDescriptionRules"
                         label="Unit Description"/>
               </v-flex>
-              <!--<v-flex>-->
-                <!--<v-combobox-->
-                        <!--:rules="chipRules"-->
-                        <!--v-model="chipModel"-->
-                        <!--:filter="filter"-->
-                        <!--:hide-no-data="!chipSearch"-->
-                        <!--:items="chipItems"-->
-                        <!--:search-input.sync="chipSearch"-->
-                        <!--hide-selected-->
-                        <!--label="Unit tags"-->
-                        <!--multiple-->
-                        <!--small-chips-->
-                        <!--solo>-->
-
-                  <!--<template v-slot:no-data>-->
-                    <!--<v-list-tile>-->
-                      <!--<span class="subheading">Create</span>-->
-                      <!--<v-chip-->
-                              <!--:color="`${chipColors[chipNonce - 1]} lighten-3`"-->
-                              <!--label-->
-                              <!--small>-->
-                        <!--{{ chipSearch }}-->
-                      <!--</v-chip>-->
-                    <!--</v-list-tile>-->
-                  <!--</template>-->
-
-                  <!--<template v-slot:selection="{ item, parent, selected }">-->
-                    <!--<v-chip-->
-                            <!--v-if="item === Object(item)"-->
-                            <!--:color="`${item.color} lighten-3`"-->
-                            <!--:selected="selected"-->
-                            <!--label-->
-                            <!--small>-->
-                              <!--<span class="pr-2">-->
-                                <!--{{ item.text }}-->
-                              <!--</span>-->
-                      <!--<v-icon-->
-                              <!--small-->
-                              <!--@click="parent.selectItem(item)">close-->
-                      <!--</v-icon>-->
-                    <!--</v-chip>-->
-                  <!--</template>-->
-
-                  <!--<template v-slot:item="{ index, item }">-->
-                    <!--<v-list-tile-content>-->
-                      <!--<v-text-field-->
-                              <!--v-if="editing === item"-->
-                              <!--v-model="editing.text"-->
-                              <!--autofocus-->
-                              <!--flat-->
-                              <!--background-color="transparent"-->
-                              <!--hide-details-->
-                              <!--solo-->
-                              <!--@keyup.enter="edit(index, item)">-->
-                      <!--</v-text-field>-->
-                      <!--<v-chip-->
-                              <!--v-else-->
-                              <!--:color="`${item.color} lighten-3`"-->
-                              <!--dark-->
-                              <!--label-->
-                              <!--small>-->
-                        <!--{{ item.text }}-->
-                      <!--</v-chip>-->
-                    <!--</v-list-tile-content>-->
-                    <!--<v-spacer></v-spacer>-->
-                    <!--<v-list-tile-action @click.stop>-->
-                      <!--<v-btn-->
-                              <!--icon-->
-                              <!--@click.stop.prevent="edit(index, item)">-->
-                        <!--<v-icon>{{ editing !== item ? 'edit' : 'check' }}</v-icon>-->
-                      <!--</v-btn>-->
-                    <!--</v-list-tile-action>-->
-                  <!--</template>-->
-
-                <!--</v-combobox>-->
-              <!--</v-flex>-->
+              <v-flex>
+                <v-combobox
+                        v-model="chipModel"
+                        :items="chipItems"
+                        :search-input.sync="chipSearch"
+                        :rules="chipRules"
+                        hide-selected
+                        hint="Maximum of 3 tags"
+                        label="Add some tags"
+                        multiple
+                        persistent-hint
+                        small-chips
+                >
+                  <template v-slot:no-data>
+                    <v-list-tile>
+                      <v-list-tile-content>
+                        <v-list-tile-title>
+                          Press <kbd>enter</kbd> to create tag "<strong>{{ chipSearch }}</strong>"
+                        </v-list-tile-title>
+                      </v-list-tile-content>
+                    </v-list-tile>
+                  </template>
+                </v-combobox>
+              </v-flex>
               <v-flex xs12 text-xs-right>
                 <v-btn
                         class="mx-0 font-weight-light"
@@ -292,46 +235,50 @@
 	  return {
 		chipRules: [
 		  chips => {
-			for (let i = 0; i < chips.length; ++i) {
-			  if (chips[i].text.length >= 5) {
-				return 'Tag name must be less than 5 symbols';
+			if (chips !== null) {
+			  for (let i = 0; i < chips.length; ++i) {
+				if (chips[i].length >= 5) {
+				  return 'Tag name must be less than 5 symbols';
+				}
 			  }
+			  return true;
 			}
-			return true;
+			return 'The field must not be empty';
 		  },
-		  chips => chips.length <= 3 || 'You can specify no more than 3 tags',
-		  v => {
-			if (v.length === 0) {
-			  return 'The field must not be empty';
-			}
-			return true;
-		  }
+		  chips => chips !== null ? (chips.length <= 3 || 'You can specify no more than 3 tags.') : 'The field must not be empty',
+		  chips => chips !== null ? (chips.length !== 0 || 'The field must not be empty') : 'The field must not be empty',
 		],
 		courseOrUnitNameRules: [
 		  v => {
-			const words = v.split(' ');
-			for (let i = 0; i < words.length; ++i) {
-			  if (words[i].length >= 12) {
-				return 'This field does not allow words longer than 12 characters.';
+			if (v !== null) {
+			  const words = v.split(' ');
+			  for (let i = 0; i < words.length; ++i) {
+				if (words[i].length >= 12) {
+				  return 'This field does not allow words longer than 12 characters.';
+				}
 			  }
+			  return true;
 			}
-			return true;
+			return 'The field must not be empty';
 		  },
 		  v => !!v || 'The field must not be empty',
-		  v => v.length <= 24 || 'Name is too long'
+		  v => v !== null ? (v.length <= 24 || 'Name is too long') : 'The field must not be empty'
 		],
 		courseOrUnitDescriptionRules: [
 		  v => {
-			const words = v.split(' ');
-			for (let i = 0; i < words.length; ++i) {
-			  if (words[i].length >= 12) {
-				return 'This field does not allow words longer than 12 characters.';
+			if (v !== null) {
+			  const words = v.split(' ');
+			  for (let i = 0; i < words.length; ++i) {
+				if (words[i].length >= 12) {
+				  return 'This field does not allow words longer than 12 characters.';
+				}
 			  }
+			  return true;
 			}
-			return true;
+			return 'The field must not be empty';
 		  },
 		  v => !!v || 'The field must not be empty',
-		  v => v.length <= 40 || 'Desctiption is too long'
+		  v => v !== null ? (v.length <= 40 || 'Desctiption is too long') : 'The field must not be empty'
 		],
 		createUnitValid: false,
 		valid: false,
@@ -389,7 +336,7 @@
 			unit_name: this.newUnitName,
 			description: this.newUnitDescription,
 			course_id: this.course.id,
-			tags: this.chipModel.map(el => el.text)
+			tags: this.chipModel
 		  };
 		  console.log('unit ', unit);
 		  this.$store.dispatch('createUnit', unit)
@@ -456,19 +403,12 @@
 	  },
 	},
 	watch: {
-	  chipModel(val, prev) {
-		if (val.length === prev.length) return;
-		this.chipModel = val.map(v => {
-		  if (typeof v === 'string') {
-			v = {
-			  text: v,
-			  color: this.chipColors[this.chipNonce - 1]
-			};
-			this.chipItems.push(v);
-			this.chipNonce++
+	  chipModel(val) {
+		if (val !== null) {
+		  if (val.length > 3) {
+			this.$nextTick(() => this.chipModel.pop())
 		  }
-		  return v
-		})
+		}
 	  }
 	},
 	created() {
