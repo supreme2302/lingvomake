01a64aa6dcf56ee2ccd45916dda15297a1473125
HimmelSpark <adam404pet@gmail.com>
2019-05-14 14:32:51 +0300

    fix

--- src/views/Help.vue
+++ src/views/Help.vue
@@ -1,7 +1,123 @@
 <template>
   <v-container fluid>
     <v-layout row>
-      <v-flex xs12>
+      <v-flex xs12 class="hidden-sm-and-up">
+
+        <h1>hidden-sm-and-up</h1>
+
+        <v-parallax
+            dark
+            src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg"
+            height="80"
+        >
+          <v-layout
+              align-center
+              column
+              justify-center
+          >
+            <h1 class="display-1 font-weight-thin mb-3">Course Management</h1>
+          </v-layout>
+        </v-parallax>
+
+        <v-timeline align-top>
+          <v-timeline-item
+              v-for="item in items"
+              :key="item.header"
+              :icon="item.icon"
+              color="orange"
+              dense
+              fill-dot>
+            <v-card
+                dark>
+              <v-card-title class="title">{{item.header}}</v-card-title>
+              <v-card-text class="white text--primary">
+                <p class="black--text headline">{{item.text}}</p>
+              </v-card-text>
+              <v-img
+                  :src="item.imgSrc"
+                  aspect-ratio="2.75"
+              ></v-img>
+            </v-card>
+          </v-timeline-item>
+        </v-timeline>
+
+        <v-parallax
+            dark
+            src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg"
+            height="80"
+        >
+          <v-layout
+              align-center
+              column
+              justify-center
+          >
+            <h1 class="display-1 font-weight-thin mb-3">Group Management</h1>
+          </v-layout>
+        </v-parallax>
+
+        <v-timeline align-top>
+          <v-timeline-item
+              v-for="item in items2"
+              :key="item.header"
+              :icon="item.icon"
+              color="orange"
+              fill-dot>
+            <v-card
+                dark>
+              <v-card-title class="title">{{item.header}}</v-card-title>
+              <v-card-text class="white text--primary">
+                <p class="black--text headline">{{item.text}}</p>
+              </v-card-text>
+              <v-img
+                  :src="item.imgSrc"
+                  aspect-ratio="2.75"
+              ></v-img>
+            </v-card>
+          </v-timeline-item>
+        </v-timeline>
+
+        <v-parallax
+            dark
+            src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg"
+            height="80"
+        >
+          <v-layout
+              align-center
+              column
+              justify-center
+          >
+            <h1 class="display-1 font-weight-thin mb-3">Application Managemen</h1>
+          </v-layout>
+        </v-parallax>
+
+        <v-timeline align-top>
+          <v-timeline-item
+              v-for="item in items3"
+              :key="item.header"
+              :icon="item.icon"
+              color="orange"
+              fill-dot>
+            <v-card
+                dark>
+              <v-card-title class="title">{{item.header}}</v-card-title>
+              <v-card-text class="white text--primary">
+                <p class="black--text headline">{{item.text}}</p>
+              </v-card-text>
+              <v-img
+                  :src="item.imgSrc"
+                  aspect-ratio="2.75"
+              ></v-img>
+            </v-card>
+          </v-timeline-item>
+        </v-timeline>
+
+        <hr>
+
+      </v-flex>
+
+      <v-flex xs12 class="hidden-xs-only">
+
+        <h1>hidden-xs-only</h1>
 
         <v-parallax
             dark
